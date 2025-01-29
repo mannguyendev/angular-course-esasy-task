6136a092b9d49a8962fcfe6ee0f091f8cf947f96
mannguyendev <mannguyen.dev@gmail.com>
2025-01-29 18:43:05 +0700

    Commit on 28-01-2025

---
+++ src/app/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: './header.component.html',
+  styles: ['h1 { color: red; }'],
+})
+export class HeaderComponent {}
