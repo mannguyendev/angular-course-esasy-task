7ecd8b7b5201743d93e7e246408fd2aedaf220c4
mannguyendev <mannguyen.dev@gmail.com>
2025-02-03 16:07:27 +0700

    Creating & Conditionlly Rendering Another Component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,16 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
+  isAddingTask = false;
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
 
   tasks = [
     {
