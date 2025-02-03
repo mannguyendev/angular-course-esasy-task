6799f6f43ce164585adc3b4030cc11602c61e2df
mannguyendev <mannguyen.dev@gmail.com>
2025-02-03 15:47:35 +0700

    More Component Communication: Deleting tasks

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -13,6 +13,7 @@ export class UserComponent {
   // @Input({ required: true }) name!: string;
 
   @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
 
   @Output() select = new EventEmitter<string>();
 
