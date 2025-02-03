207c39b642edecb242b21704e3ce745f1212d2d5
mannguyendev <mannguyen.dev@gmail.com>
2025-02-03 15:06:19 +0700

    Outputting User-specific Tasks

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -9,5 +9,37 @@ import { TaskComponent } from './task/task.component';
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  @Input({ required: true }) name?: string;
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name!: string;
+
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nibh. Etiam non elit dui. Nulla nec purus',
+      dueDate: '2025-12-29',
+    },
+    {
+      id: 't2',
+      userId: 'u2',
+      title: 'Master Angular 1',
+      summary:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nibh. Etiam non elit dui. Nulla nec purus 1',
+      dueDate: '2025-12-30',
+    },
+    {
+      id: 't3',
+      userId: 'u1',
+      title: 'Master Angular 2',
+      summary:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nibh. Etiam non elit dui. Nulla nec purus 2',
+      dueDate: '2025-12-31',
+    },
+  ];
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
 }
