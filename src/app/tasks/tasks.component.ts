bada628db82cf0001a21cce43a3a91a9766c6226
mannguyendev <mannguyen.dev@gmail.com>
2025-02-04 16:33:58 +0700

    Practice Service

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -26,10 +26,6 @@ export class TasksComponent {
     return this.tasksService.getUserTasks(this.userId);
   }
 
-  onCompleteTask(id: string) {
-    this.tasksService.removeTask(id);
-  }
-
   onCloseAddTask() {
     this.isAddingTask = false;
   }
