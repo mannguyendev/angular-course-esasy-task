3dfab82517735f92ce1fdb82297cbe20251d20c7
mannguyendev <mannguyen.dev@gmail.com>
2025-02-04 16:14:03 +0700

    Getting Started with Dependency Injection

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { NewTaskComponent } from './new-task/new-task.component';
 import { type NewTaskData } from './task/task.model';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -15,43 +16,18 @@ export class TasksComponent {
   @Input({ required: true }) name!: string;
   isAddingTask = false;
 
+  constructor(private tasksService: TasksService) {}
+
   onStartAddTask() {
     this.isAddingTask = true;
   }
 
-  tasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nibh. Etiam non elit dui. Nulla nec purus',
-      dueDate: '2025-12-29',
-    },
-    {
-      id: 't2',
-      userId: 'u2',
-      title: 'Master Angular 1',
-      summary:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nibh. Etiam non elit dui. Nulla nec purus 1',
-      dueDate: '2025-12-30',
-    },
-    {
-      id: 't3',
-      userId: 'u1',
-      title: 'Master Angular 2',
-      summary:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nibh. Etiam non elit dui. Nulla nec purus 2',
-      dueDate: '2025-12-31',
-    },
-  ];
-
   get selectedUserTasks() {
-    return this.tasks.filter((task) => task.userId === this.userId);
+    return this.tasksService.getUserTasks(this.userId);
   }
 
   onCompleteTask(id: string) {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.tasksService.removeTask(id);
   }
 
   onCancelAddTask() {
@@ -59,13 +35,7 @@ export class TasksComponent {
   }
 
   onAddTask(taskData: NewTaskData) {
-    this.tasks.unshift({
-      id: new Date().getTime().toString(),
-      userId: this.userId,
-      title: taskData.title,
-      summary: taskData.summary,
-      dueDate: taskData.date,
-    });
+    this.tasksService.addTask(taskData, this.userId);
     this.isAddingTask = false;
   }
 }
---
+++ src/app/tasks/tasks.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { NewTaskData } from './task/task.model';
+
+@Injectable({ providedIn: 'root' })
+export class TasksService {
+  private tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nibh. Etiam non elit dui. Nulla nec purus',
+      dueDate: '2025-12-29',
+    },
+    {
+      id: 't2',
+      userId: 'u2',
+      title: 'Master Angular 1',
+      summary:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nibh. Etiam non elit dui. Nulla nec purus 1',
+      dueDate: '2025-12-30',
+    },
+    {
+      id: 't3',
+      userId: 'u1',
+      title: 'Master Angular 2',
+      summary:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nibh. Etiam non elit dui. Nulla nec purus 2',
+      dueDate: '2025-12-31',
+    },
+  ];
+
+  getUserTasks(userId: string) {
+    return this.tasks.filter((task) => task.userId === userId);
+  }
+
+  addTask(taskData: NewTaskData, userId: string) {
+    this.tasks.unshift({
+      id: new Date().getTime().toString(),
+      userId: userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date,
+    });
+  }
+
+  removeTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+}
